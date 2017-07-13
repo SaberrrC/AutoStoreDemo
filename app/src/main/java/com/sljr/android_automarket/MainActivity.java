07573cb2fcf81a318757c863988f1f87da44cd73
wr <aronwang@foxmail.com>
2017-07-13 17:00:16 +0800

    [wangrui] 创建工程

---
+++ app/src/main/java/com/sljr/android_automarket/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sljr.android_automarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
