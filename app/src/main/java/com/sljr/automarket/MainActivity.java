c83df9c7297bd4065df01100e6361aad3277797a
wr <aronwang@foxmail.com>
2017-07-16 09:43:24 +0800

    [wangrui] no message

--- app/src/main/java/com/sljr/automarket/MainActivity.java
+++ app/src/main/java/com/sljr/automarket/MainActivity.java
@@ -1,15 +1,10 @@
 package com.sljr.automarket;
 
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.sljr.automarket.base.BaseActivity;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends BaseActivity {
 
@@ -32,24 +27,6 @@ public class MainActivity extends BaseActivity {
     @Override
     public void initData() {
 
-    }
-
-    public void test(View view) {
-        RetrofitUtils<ObjDataBean> netUtil = new RetrofitUtils();
-
-        Call<ObjDataBean> call = netUtil.ObjGet("mock");
-        call.enqueue(new Callback<ObjDataBean>() {
-            @Override
-            public void onResponse(Call<ObjDataBean> call, Response<ObjDataBean> response) {
-                ObjDataBean body = response.body();
-                Log.d(TAG, "onResponse: "+body);
-            }
-
-            @Override
-            public void onFailure(Call<ObjDataBean> call, Throwable t) {
-                Log.d(TAG, "onFailure: ");
-            }
-        });
     }
 
     @Override
