29786be3fb9bd50f4400297aaf2504a99231407d
wr <aronwang@foxmail.com>
2017-07-17 13:12:02 +0800

    [wangrui]提交部分代码

--- app/src/main/java/com/shanlin/autostore/MainActivity.java
+++ app/src/main/java/com/shanlin/autostore/MainActivity.java
@@ -1,16 +1,22 @@
 package com.shanlin.autostore;
 
 import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.shanlin.autostore.base.BaseActivity;
+import com.shanlin.autostore.fragment.BuyRecordFragment;
+import com.shanlin.autostore.fragment.MainFragment;
+import com.shanlin.autostore.fragment.VersionInfoFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
 
@@ -19,6 +25,7 @@ public class MainActivity extends BaseActivity {
     private DrawerLayout mDrawerLayout;
     private TextView toolbar_title;
     private Toolbar toolbar;
+    private List<Fragment> fragments = new ArrayList<>();
 
 
     @Override
@@ -35,6 +42,7 @@ public class MainActivity extends BaseActivity {
     @Override
     public void initData() {
         initToolBar();
+        initFragments();
     }
 
     private void initToolBar() {
@@ -51,6 +59,7 @@ public class MainActivity extends BaseActivity {
                 super.onDrawerOpened(drawerView);
 
             }
+
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
@@ -69,13 +78,6 @@ public class MainActivity extends BaseActivity {
          mDrawerLayout.findViewById(R.id.location_2).setOnClickListener(this);
          mDrawerLayout.findViewById(R.id.location_3).setOnClickListener(this);
          mDrawerLayout.findViewById(R.id.location_4).setOnClickListener(this);
-        ListView buyRecordLV = (ListView) mDrawerLayout.findViewById(R.id.mian_lv);
-        buyRecordLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                
-            }
-        });
     }
 
     @Override
@@ -84,15 +86,68 @@ public class MainActivity extends BaseActivity {
         switch (v.getId()) {
 
             case R.id.location_2:
-                toolbar_title.setText("购买记录");
-                toolbar_title.setTextColor(Color.BLACK);
-                toolbar.setNavigationIcon(R.mipmap.nav_back);
-                mDrawerLayout.closeDrawer(Gravity.LEFT);
+                chooseFragement("购买记录",1);
                 break;
             case R.id.location_3:
+                chooseFragement("版本信息",2);
                 break;
             case R.id.location_4:
                 break;
         }
     }
+
+    private void chooseFragement(String title,int index) {
+        toolbar_title.setText(title);
+        toolbar_title.setTextColor(Color.BLACK);
+        toolbar.setNavigationIcon(R.mipmap.nav_back);
+        mDrawerLayout.closeDrawer(Gravity.LEFT);
+        changeFrag(index);
+    }
+
+    /**
+     * 初始化fargmenst
+     */
+    private void initFragments() {
+        fragments.add(new MainFragment());
+        fragments.add(new BuyRecordFragment());
+        fragments.add(new VersionInfoFragment());
+        changeFrag(0);
+    }
+
+    private void changeFrag(int curIndex) {
+        for (int i = 0; i < fragments.size(); i++) {
+            if (i == curIndex) {
+                showFragment(fragments.get(i));
+            } else {
+                hideFragment(fragments.get(i));
+            }
+        }
+    }
+
+
+    protected void hideFragment(Fragment currFragment) {
+        if (currFragment == null)
+            return;
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        currFragment.onPause();
+        if (currFragment.isAdded()) {
+            transaction.hide(currFragment);
+            transaction.commitAllowingStateLoss();
+        }
+    }
+
+    protected void showFragment(Fragment startFragment) {
+        if (startFragment == null)
+            return;
+        FragmentTransaction startFragmentTransaction = getSupportFragmentManager()
+                .beginTransaction();
+        if (!startFragment.isAdded()) {
+            startFragmentTransaction.add(R.id.container, startFragment);
+        } else {
+            startFragment.onResume();
+            startFragmentTransaction.show(startFragment);
+        }
+        startFragmentTransaction.commitAllowingStateLoss();
+    }
+
 }
