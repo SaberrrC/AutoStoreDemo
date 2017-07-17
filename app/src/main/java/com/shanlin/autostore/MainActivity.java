ac92d8ad2a2ad3746b00be353eb0438a2dcaf444
wr <aronwang@foxmail.com>
2017-07-17 10:43:05 +0800

    [wangrui]提交部分代码

--- app/src/main/java/com/shanlin/autostore/MainActivity.java
+++ app/src/main/java/com/shanlin/autostore/MainActivity.java
@@ -1,9 +1,14 @@
 package com.shanlin.autostore;
 
+import android.graphics.Color;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.shanlin.autostore.base.BaseActivity;
 
@@ -12,6 +17,8 @@ public class MainActivity extends BaseActivity {
     private ActionBarDrawerToggle mDrawerToggle;
     private static final String TAG = "wr";
     private DrawerLayout mDrawerLayout;
+    private TextView toolbar_title;
+    private Toolbar toolbar;
 
 
     @Override
@@ -22,6 +29,7 @@ public class MainActivity extends BaseActivity {
     @Override
     public void initView() {
         mDrawerLayout = ((DrawerLayout) findViewById(R.id.activity_main));
+        toolbar_title = ((TextView) findViewById(R.id.toolbar_title));
     }
 
     @Override
@@ -30,7 +38,7 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initToolBar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
@@ -51,10 +59,40 @@ public class MainActivity extends BaseActivity {
         };
         mDrawerToggle.syncState();
         mDrawerLayout.setDrawerListener(mDrawerToggle);
+        drawerSetting();
+    }
+
+    /**
+     * 抽屉设置
+     */
+    private void drawerSetting() {
+         mDrawerLayout.findViewById(R.id.location_2).setOnClickListener(this);
+         mDrawerLayout.findViewById(R.id.location_3).setOnClickListener(this);
+         mDrawerLayout.findViewById(R.id.location_4).setOnClickListener(this);
+        ListView buyRecordLV = (ListView) mDrawerLayout.findViewById(R.id.mian_lv);
+        buyRecordLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
 
+        switch (v.getId()) {
+
+            case R.id.location_2:
+                toolbar_title.setText("购买记录");
+                toolbar_title.setTextColor(Color.BLACK);
+                toolbar.setNavigationIcon(R.mipmap.nav_back);
+                mDrawerLayout.closeDrawer(Gravity.LEFT);
+                break;
+            case R.id.location_3:
+                break;
+            case R.id.location_4:
+                break;
+        }
     }
 }
