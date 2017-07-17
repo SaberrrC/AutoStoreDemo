6cb9d832e6499ca561ecfda9f62dbe5d5621e0ef
shanlin_chumingyuan <saberrrc@163.com>
2017-07-17 13:34:21 +0800

    [mingyuan.chu]修改扫描页面

---
+++ app/src/main/java/com/shanlin/autostore/activity/GateActivity.java
@@ -0,0 +1,28 @@
+package com.shanlin.autostore.activity;
+
+import android.view.View;
+
+import com.shanlin.autostore.R;
+import com.shanlin.autostore.base.BaseActivity;
+
+public class GateActivity extends BaseActivity {
+
+    @Override
+    public int initLayout() {
+        return R.layout.activity_gate;
+    }
+
+    @Override
+    public void initView() {
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
