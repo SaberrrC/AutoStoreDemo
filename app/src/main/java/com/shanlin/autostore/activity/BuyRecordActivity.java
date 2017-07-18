160b8fca7282c6e259fc5e9a0e111d8c1f6872b3
shanlin_chumingyuan <saberrrc@163.com>
2017-07-18 14:51:25 +0800

    [mingyuan.chu]导入下拉刷新控件

--- app/src/main/java/com/shanlin/autostore/activity/BuyRecordActivity.java
+++ app/src/main/java/com/shanlin/autostore/activity/BuyRecordActivity.java
@@ -1,25 +1,41 @@
 package com.shanlin.autostore.activity;
 
 import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.shanlin.autostore.MainActivity;
 import com.shanlin.autostore.R;
+import com.shanlin.autostore.adapter.FinalRecycleAdapter;
 import com.shanlin.autostore.base.BaseActivity;
-import com.shanlin.autostore.utils.CommonUtils;
+import com.shanlin.autostore.bean.RecordBean;
+import com.shanlin.autostore.utils.ThreadUtils;
+import com.shanlin.autostore.view.PulltoRefreshRecyclerView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by DELL on 2017/7/17 0017.
  */
 
-public class BuyRecordActivity extends BaseActivity {
+public class BuyRecordActivity extends BaseActivity implements FinalRecycleAdapter.OnViewAttachListener {
 
-    private ListView buyRecordLV;
-    private Toolbar toolbar;
-    private TextView title;
+    private ListView                  buyRecordLV;
+    private Toolbar                   toolbar;
+    private TextView                  title;
+    private PulltoRefreshRecyclerView mPulltoRefreshRecyclerView;
+    private RecyclerView              mRecyclerView;
+    private List<Object> mDatas = new ArrayList<>();
+    private FinalRecycleAdapter mFinalRecycleAdapter;
+    private static int REFRESH       = 0;
+    private static int LOAD          = 1;
+    private        int currentAction = 0;//记录当前用户手势是下拉刷新还是上拉更多，默认下拉刷新
+    private        int pageno        = 1;
 
 
     @Override
@@ -29,20 +45,67 @@ public class BuyRecordActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        buyRecordLV = ((ListView) findViewById(R.id.lv_buy_record));
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         title = ((TextView) findViewById(R.id.toolbar_title));
         title.setText("购买记录");
         title.setTextColor(Color.BLACK);
         toolbar.setNavigationIcon(R.mipmap.nav_back);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CommonUtils.toNextActivity(v.getContext(), MainActivity.class);
-                finish();
-            }
-        });
+        toolbar.setNavigationOnClickListener(this);
+        mPulltoRefreshRecyclerView = (PulltoRefreshRecyclerView) findViewById(R.id.pr_lists);
+        mRecyclerView = mPulltoRefreshRecyclerView.getRecyclerView();
+        Map<Class, Integer> map = new HashMap<>();
+        map.put(RecordBean.class, R.layout.item_record);
+        for (int i = 0; i < 10; i++) {
+            mDatas.add(new RecordBean());
+        }
+        mFinalRecycleAdapter = new FinalRecycleAdapter(mDatas, map, this);
+        mRecyclerView.setAdapter(mFinalRecycleAdapter);
+        mPulltoRefreshRecyclerView.setRefreshLoadMoreListener(MyRefreshLoadMoreListener);
     }
+
+
+    /**
+     * 下拉和上拉的监听函數
+     */
+    private PulltoRefreshRecyclerView.RefreshLoadMoreListener MyRefreshLoadMoreListener = new PulltoRefreshRecyclerView.RefreshLoadMoreListener() {
+        @Override
+        public void onRefresh() {
+            //            // TODO: 2017-5-31
+            currentAction = REFRESH;
+            pageno = 1;
+            ThreadUtils.runMainDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //                    if (SLJRApplication.isLogin) {
+                    //                        getShoppingList();
+                    //                    } else {
+                    //                        toLoginActivity();
+                    //                    }
+                    mPulltoRefreshRecyclerView.stopRefresh();
+                }
+            }, 500);
+        }
+
+        @Override
+        public void onLoadMore() {
+            currentAction = LOAD;
+            //            LogUtils.d(currentPage + "--------------" + "totalPage--------------------" + +totalPage);
+            //            if (currentPage < totalPage) {
+            //                pageno++;
+            //                getShoppingList();
+            //            } else {
+            //                T.showShort("没有更多数据！");
+            //            }
+            //获取数据
+            ThreadUtils.runMainDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //拿到数据了
+                    mPulltoRefreshRecyclerView.setLoadMoreCompleted();
+                }
+            }, 500);
+        }
+    };
 
     @Override
     public void initData() {
@@ -51,6 +114,16 @@ public class BuyRecordActivity extends BaseActivity {
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.toolbar:
+                finish();
+                break;
+
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(FinalRecycleAdapter.ViewHolder holder, int position, Object itemData) {
 
     }
 }
