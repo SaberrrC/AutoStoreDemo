2cfdc1e6bc04cdd995c4f8184309fb6e0f4cee91
wr <aronwang@foxmail.com>
2017-07-28 10:40:37 +0800

    [wangrui]增加开通乐买宝部分功能

---
+++ app/src/main/java/com/shanlin/autostore/bean/RealNameAuthenBean.java
@@ -0,0 +1,33 @@
+package com.shanlin.autostore.bean;
+
+/**
+ * Created by DELL on 2017/7/27 0027.
+ */
+
+public class RealNameAuthenBean {
+
+
+    /**
+     * code : 200
+     * message : 实名认证成功
+     */
+
+    private String code;
+    private String message;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ app/src/main/java/com/shanlin/autostore/constants/Constant_LeMaiBao.java
@@ -0,0 +1,20 @@
+package com.shanlin.autostore.constants;
+
+/**
+ * Created by DELL on 2017/7/27 0027.
+ */
+
+public class Constant_LeMaiBao {
+
+    /**
+     * 是否已经实名认证
+     */
+    public static final String AUTHEN_STATE_KEY = "authen_state";
+
+    /**
+     * 认证状态 0未认证 , 1 已实名认证, 2 已认证
+     */
+    public static final String AUTHEN_FINISHED = "2";
+    public static final String AUTHEN_NOT = "0";
+    public static final String AUTHEN_REAL_NAME = "1";
+}
