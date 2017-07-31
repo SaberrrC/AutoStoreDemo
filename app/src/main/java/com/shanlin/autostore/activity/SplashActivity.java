61e02fe754162a227775a5752f1b46483e403973
zouchangcheng <648488475@qq.com>
2017-07-31 11:13:57 +0800

    splash更新

--- app/src/main/java/com/shanlin/autostore/activity/SplashActivity.java
+++ app/src/main/java/com/shanlin/autostore/activity/SplashActivity.java
@@ -4,12 +4,15 @@ import android.Manifest;
 import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.animation.LinearInterpolator;
 
 import com.shanlin.autostore.R;
@@ -22,6 +25,7 @@ import com.shanlin.autostore.utils.LogUtils;
 import com.shanlin.autostore.utils.MPermissionUtils;
 import com.shanlin.autostore.utils.SpUtils;
 import com.shanlin.autostore.utils.StatusBarUtils;
+import com.shanlin.autostore.utils.VersionManagementUtil;
 
 import retrofit2.Call;
 
@@ -32,16 +36,19 @@ import retrofit2.Call;
 public class SplashActivity extends Activity {
 
 
+    private AlertDialog updateDialog;
+    private int forceUpdate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         StatusBarUtils.setColor(this, Color.TRANSPARENT);
         LogUtils.d("token  " + SpUtils.getString(this, Constant.TOKEN, ""));
-        MPermissionUtils.requestPermissionsResult(this, 1, new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_CONTACTS}, new MPermissionUtils.OnPermissionListener() {
+        MPermissionUtils.requestPermissionsResult(this, 1, new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_CONTACTS}, new MPermissionUtils.OnPermissionListener() {
             @Override
             public void onPermissionGranted() {
-                loadAnim();
+                checkUpdate();
             }
 
             @Override
@@ -49,7 +56,21 @@ public class SplashActivity extends Activity {
                 MPermissionUtils.showTipsDialog(SplashActivity.this);
             }
         });
-        checkUpdate();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (updateDialog != null)
+            updateDialog.dismiss();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if (forceUpdate != 1)
+            loadAnim();
     }
 
     private void checkUpdate() {
@@ -61,14 +82,23 @@ public class SplashActivity extends Activity {
                 //成功
                 if (data != null) {
                     try {
-                        PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-                        String version = data.getVersion();
-                        // TODO: 2017-7-27 版本 0.1.1
-//                        if (version != null && Integer.valueOf(version).intValue() > packageInfo.versionCode) {
-//                            showUpdateDialog(data.getForceUpdate(), data.getDownloadUrl());
-//                        }
                         //判断是否更新
-                    } catch (PackageManager.NameNotFoundException e) {
+                        // TODO: 2017-7-27 版本 0.1.1
+                        String currentVersion = VersionManagementUtil.getVersion(SplashActivity.this);
+                        String version = data.getVersion();
+                        forceUpdate = data.getForceUpdate();
+                        if (version != null) {
+                            //比较版本，返回1需要更新
+                            if (VersionManagementUtil.VersionComparison(version, currentVersion) == 1) {
+                                //更新
+                                showUpdateDialog(data.getForceUpdate(), data.getDownloadUrl());
+                            } else {
+                                loadAnim();
+                            }
+                        } else
+                            loadAnim();
+                        //不更新
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
@@ -76,33 +106,71 @@ public class SplashActivity extends Activity {
 
             @Override
             public void error(Throwable ex, String code, String msg) {
-
+                MPermissionUtils.showTipsDialog(SplashActivity.this);
             }
         });
 
     }
 
-    private void showUpdateDialog(int forceUpdate, String DownLoadUrl) {
-        AlertDialog.Builder adInfo = new AlertDialog.Builder(SplashActivity.this);
+    private void upData(String url) {
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.VIEW");
+        Uri content_url = Uri.parse(url);
+        intent.setData(content_url);
+        try {
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showUpdateDialog(int forceUpdate, final String DownLoadUrl) {
+        final AlertDialog.Builder adInfo = new AlertDialog.Builder(SplashActivity.this);
         switch (forceUpdate) {
             //强更
+            case 1:
+                MPermissionUtils.requestPermissionsResult(this, 1, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, new MPermissionUtils.OnPermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+                        adInfo.setTitle("版本更新");
+                        adInfo.setMessage("此版本不可用，请更新");
+                        updateDialog = adInfo.setPositiveButton("更新", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // TODO: 2017/7/26  更新， 进行web跳转 下载apk
+                                upData(DownLoadUrl);
+                            }
+                        }).create();
+                        updateDialog.setCancelable(false);
+                        updateDialog.setCanceledOnTouchOutside(false);
+                        updateDialog.show();
+                    }
+
+                    @Override
+                    public void onPermissionDenied() {
+
+                    }
+                });
+                break;
+            //非强更
             case 0:
                 adInfo.setTitle("版本更新");
-                adInfo.setMessage("此版本不可用，请更新");
-                AlertDialog updateDialog = adInfo.setPositiveButton("更新", new DialogInterface.OnClickListener() {
+                adInfo.setMessage("发现新版本，请更新");
+                updateDialog = adInfo.setPositiveButton("更新", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // TODO: 2017/7/26  更新， 进行webView跳转 下载apk
+                        // TODO: 2017/7/26  更新， 进行web跳转 下载apk
+                        upData(DownLoadUrl);
+                    }
+                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        loadAnim();
                     }
                 }).create();
                 updateDialog.setCancelable(false);
                 updateDialog.setCanceledOnTouchOutside(false);
                 updateDialog.show();
-                break;
-            //非强更
-            case 1:
-                break;
-            default:
                 break;
         }
     }
