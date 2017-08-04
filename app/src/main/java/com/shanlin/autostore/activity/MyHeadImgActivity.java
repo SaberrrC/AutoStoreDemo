42303854e3cc5bda202be6a5120ef210c25c862a
wr <aronwang@foxmail.com>
2017-08-04 17:37:24 +0800

    [wangrui] 接入相册功能

--- app/src/main/java/com/shanlin/autostore/activity/MyHeadImgActivity.java
+++ app/src/main/java/com/shanlin/autostore/activity/MyHeadImgActivity.java
@@ -1,6 +1,8 @@
 package com.shanlin.autostore.activity;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,30 +11,40 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 
+import com.jph.takephoto.app.TakePhotoActivity;
+import com.jph.takephoto.model.TImage;
+import com.jph.takephoto.model.TResult;
 import com.shanlin.autostore.MainActivity;
 import com.shanlin.autostore.R;
-import com.shanlin.autostore.base.BaseActivity;
+import com.shanlin.autostore.takephoto.CustomHelper;
+import com.shanlin.autostore.takephoto.ResultActivity;
 import com.shanlin.autostore.utils.CommonUtils;
 
 /**
  * Created by DELL on 2017/8/2 0002.
  */
 
-public class MyHeadImgActivity extends BaseActivity {
+public class MyHeadImgActivity extends TakePhotoActivity implements View.OnClickListener {
 
     private PopupWindow popHeadChoose;
+    private CustomHelper customHelper;
+    private ImageView imgLarge;
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(initLayout());
+        initView();
+    }
+
     public int initLayout() {
         return R.layout.activity_my_head_img;
     }
 
-    @Override
     public void initView() {
         CommonUtils.initToolbar(this,"我的头像", R.color.blcak, MainActivity.class);
         ((Button) findViewById(R.id.btn_change_head_img)).setOnClickListener(this);
-        ImageView imgLarge = (ImageView) findViewById(R.id.iv_head_img_large);
-        imgLarge.setOnClickListener(this);
+        imgLarge = (ImageView) findViewById(R.id.iv_head_img_large);
         initPop();
     }
 
@@ -48,6 +60,7 @@ public class MyHeadImgActivity extends BaseActivity {
         popHeadChoose.setBackgroundDrawable(new ColorDrawable(0x00000000));
         popHeadChoose.setOutsideTouchable(false);
         popHeadChoose.setFocusable(false);
+        customHelper = CustomHelper.of(headChoose);
         popHeadChoose.setOnDismissListener(new PopupWindow.OnDismissListener() {
             @Override
             public void onDismiss() {
@@ -56,35 +69,42 @@ public class MyHeadImgActivity extends BaseActivity {
         });
     }
 
-    @Override
-    public void initData() {
 
+    @Override
+    public void takeSuccess(TResult result) {
+        super.takeSuccess(result);
+//        Glide.with(this).load(new File(result.getImage().getCompressPath())).into(imgLarge);
+    }
+
+    @Override
+    public void takeFail(TResult result, String msg) {
+        super.takeFail(result, msg);
+        CommonUtils.debugLog(msg);
+    }
+
+    @Override
+    public void takeCancel() {
+        super.takeCancel();
+    }
+
+    private void showImg(TImage image) {
+        Intent intent=new Intent(this,ResultActivity.class);
+        intent.putExtra("image",image);
+        startActivity(intent);
     }
 
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()) {
-
-            case R.id.btn_change_head_img:
-                showPopWindow();
-                break;
-            //拍照
-            case R.id.tv_head_pop_camera:
-
-                break;
-            //从相册中选
-            case R.id.tv_head_pop_book:
-
-                break;
-            //拍照
-            case R.id.tv_head_pop_concel:
-                if (popHeadChoose.isShowing()) {
-                    popHeadChoose.dismiss();
-                }
-                break;
+        if (v.getId() == R.id.btn_change_head_img) {
+            showPopWindow();
+        } else if (v.getId() == R.id.tv_head_pop_concel) {
+            if (popHeadChoose.isShowing()) {
+                popHeadChoose.dismiss();
+            }
+        } else {
+            customHelper.onClick(v,getTakePhoto());
         }
-
     }
 
     private void showPopWindow() {
