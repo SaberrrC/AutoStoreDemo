35b788c28e01388adfbeccdd306409d2033658db
v_chumingyuan <saberrrc@163.com>
2017-09-25 10:22:06 +0800

    [mingyuan.chu]删除无用代码

--- app/src/main/java/com/shanlin/android/autostore/common/base/BaseActivity.java
+++ app/src/main/java/com/shanlin/android/autostore/common/base/BaseActivity.java
@@ -8,11 +8,11 @@ import android.support.annotation.Nullable;
 
 import com.shanlin.android.autostore.App;
 import com.shanlin.android.autostore.common.utils.MPermissionUtils;
+import com.shanlin.android.autostore.common.utils.StatusBarUtils;
 import com.shanlin.android.autostore.di.component.ActivityComponent;
 import com.shanlin.android.autostore.di.component.DaggerActivityComponent;
 import com.shanlin.android.autostore.di.module.ActivityModule;
 import com.shanlin.autostore.R;
-import com.shanlin.android.autostore.common.utils.StatusBarUtils;
 import com.zhy.autolayout.AutoLayoutActivity;
 
 import java.util.ArrayList;
