d7de41ac677688267947f97546df4760533ef3eb
huayuyin <huayuyin@shanlinjinrong.com>
2017-10-11 16:28:17 +0800

    update host

--- app/src/main/java/com/shanlin/android/autostore/common/constants/Constant.java
+++ app/src/main/java/com/shanlin/android/autostore/common/constants/Constant.java
@@ -12,13 +12,13 @@ import java.io.File;
 public class Constant {
     /**
      * 接口base url
-     * 开发联调：http://linstore-dev.shanlinjinrong.online:8080/linstore/v1/
-     * 测试地址：http://linstore-uat.shanlinjinrong.online:8080/linstore/v1/
+     * 开发联调：http://linstore-dev.shanlinjinrong.online/linstore/v1/
+     * 测试地址：http://linstore-uat.shanlinjinrong.online/linstore/v1/
      */
     //开发联调
-//        public static final String BASE_URL = "http://linstore-dev.shanlinjinrong.online:8080/linstore/v1/";
+//        public static final String BASE_URL = "http://linstore-dev.shanlinjinrong.online/linstore/v1/";
     //    测试地址
-    public static final String BASE_URL = "http://linstore-uat.shanlinjinrong.online:8080/linstore/v1/";
+    public static final String BASE_URL = "http://linstore-uat.shanlinjinrong.online/linstore/v1/";
     public static final String HEAD_IMG_URL = "head_url";
     public static final String TOKEN              = "TOKEN";
     public static final String USER_PHONE_LOGINED = "USER_PHONE_LOGINED";
